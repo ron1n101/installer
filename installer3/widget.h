8fd8d48a30543c9d8b9a8f204d076b930d3481d6
ron1n101 <daticob@gmail.com>
2023-12-09 00:57:40 +0200

    add std::unordered_map DownloadApp for key-word collection. Add DownloadedApp struct. Add std::set for selectedApps.

--- installer3/widget.h
+++ installer3/widget.h
@@ -3,20 +3,29 @@
 
 
 #include "downloader.h"
-
 #include <unordered_map>
+#include <set>
+
 
 #include <QWidget>
 #include <QCheckBox>
 #include <QList>
 #include <QProcess>
-#include <QMap>
+#include <QDesktopServices>
+// #include <QMap>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class Widget;
 }
 QT_END_NAMESPACE
+
+
+struct DownloadedApp
+{
+    QString link;
+    QString pathApp;
+};
 
 class Widget : public QWidget
 {
@@ -28,13 +37,13 @@ public:
 
 
 private slots:
-    void onAcrobatReader_checkBox();
-    void onAnyDesk_checkBox();
-    void onJava_checkBox();
-    void onShipSure_checkBox();
-    void onTeams_checkBox();
-    void onZip_checkBox();
-    void onGoogleChrome_checkBox();
+    // void onAcrobatReader_checkBox();
+    // void onAnyDesk_checkBox();
+    // void onJava_checkBox();
+    // void onShipSure_checkBox();
+    // void onTeams_checkBox();
+    // void onZip_checkBox();
+    // void onGoogleChrome_checkBox();
     void onSelectAll_checkBox();
 
     void onSelectTargetFolder();
@@ -51,14 +60,14 @@ private slots:
 
 private:
     QList<QCheckBox *> checkBoxes;
-    QList<QString> selectedApps;
+    // QList<QString> selectedApps;
+    std::set<QString> selectedApps;
     QString targetFolder;
     Ui::Widget *ui;
     void downloadApplication(const QString &appName, const QString &downloadUrl);
     QList<Downloader*> downloaders;
-    QString installPath;
     void InstallerRun(const QString &appName);
 
-    QMap<QString, QString> fileMapping;     // not working
+    static std::unordered_map<QString, DownloadedApp> fileMapping;
 };
 #endif // WIDGET_H
